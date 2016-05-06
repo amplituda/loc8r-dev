a5b7103a566e6725966e52c57572a787d77efbd9
Alex Krez <aleksandr.kr.26@googlemail.com>
2016-05-06 18:29:57 +0200

    updated app

--- app_client/app.js
+++ app_client/app.js
@@ -19,8 +19,17 @@
                 controller: 'locationDetailCtrl',
                 controllerAs: 'vm'
             })
-
-        .otherwise({ redirectTo: '/' });
+            .when('/register', {
+                templateUrl: '/auth/register/register.view.html',
+                controller: 'registerCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/login', {
+                templateUrl: '/auth/login/login.view.html',
+                controller: 'loginCtrl',
+                controllerAs: 'vm'
+            })
+            .otherwise({ redirectTo: '/' });
 
         $locationProvider.html5Mode(true);
 
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ start server: npm start
 
 local: localhost:3000
 
+heroku config:set JWT_SECRET=xxxxYY
